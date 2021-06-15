b66dd7ec49b6ab7d524908a4a1e0445c702cc642
Zahar Toroschin <z.toroschin@gmail.com>
2021-06-15 18:15:51 +0300

    Replace war package to jar

--- carService/src/main/java/ru/itmo/carService/CarServiceApplication.java
+++ carService/src/main/java/ru/itmo/carService/CarServiceApplication.java
@@ -2,10 +2,10 @@ package ru.itmo.carService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
-@EnableDiscoveryClient
 public class CarServiceApplication {
 
 	public static void main(String[] args) {
