7a9146e74120118bf8ca6b554ae999f94fab1cb4
v-cognitio <sivirin.v2000@yandex.ru>
2021-06-22 15:28:16 +0300

    Fill populate.sql

---
+++ tripService/src/main/resources/db/populate.sql
@@ -0,0 +1,5 @@
+INSERT INTO cars (id, name, status, latitude, longitude)
+VALUES (1, 'name', 'VACANT', 100.1, 100.2);
+
+INSERT INTO users(id, name, email, password)
+VALUES(1, 'name', 'email', 'password')
