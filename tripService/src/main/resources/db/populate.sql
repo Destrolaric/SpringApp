b2d7ddcfca08e1c12ea24e7df633a0e6e07c2f9d
Destolaric <georgi_spb_2000@mail.ru>
2021-06-24 11:00:32 +0300

    removed "accidental" Monty Python reference

--- tripService/src/main/resources/db/populate.sql
+++ tripService/src/main/resources/db/populate.sql
@@ -4,7 +4,7 @@ VALUES (1, 'name','surname','plate' ,'VACANT', 777.1, 777.2),
        (3, 'name','surname','plate' ,'VACANT', 500.1, 200.2),
        (4, 'name','surname','plate' ,'VACANT', 100.1, 110.2),
        (5, 'name','surname','plate', 'BUSY', 100.1, 100.2),
-       (6, 'name','surname','plate','biggusDickus', 'INACTIVE', 200.1, 200.2);
+       (6, 'name','surname','plate', 'INACTIVE', 200.1, 200.2);
 
 INSERT INTO users(id, name, email, password)
 VALUES(1, 'name', 'surname' , 'email', 'password')
